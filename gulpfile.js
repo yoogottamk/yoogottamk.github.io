02449b65eb9e7ada59ff9ff4196dae911da24b39
yoogottamk <yoogottamk@outlook.com>
2021-09-16 12:00:13 +0530

    random improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,42 @@
 "use strict";
 
-let gulp = require("gulp"),
-    autoprefixer = require("gulp-autoprefixer"),
-    cleanCSS = require("gulp-clean-css"),
-    ghPages = require("gulp-gh-pages"),
-    terser = require("gulp-terser");
+const gulp = require("gulp"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cleanCSS = require("gulp-clean-css"),
+  ghPages = require("gulp-gh-pages"),
+  terser = require("gulp-terser"),
+  removeEmptyLines = require("gulp-remove-empty-lines");
 
-gulp.task("styles", () => {
-    return gulp
-        .src("./_site/css/*.css", { base: "./" })
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("."));
-});
+// fix css
+gulp.task("styles", () =>
+  gulp
+    .src("./_site/css/*.css", { base: "./" })
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(".")));
 
-gulp.task("scripts", () => {
-    return gulp
-        .src("./_site/js/*.js", { base: "./" })
-        .pipe(terser({ toplevel: true }))
-        .pipe(gulp.dest("."));
-});
+// fix js
+gulp.task("scripts", () =>
+  gulp
+    .src("./_site/js/*.js", { base: "./" })
+    .pipe(terser({ toplevel: true }))
+    .pipe(gulp.dest(".")));
 
-gulp.task("ghPages", () => gulp.src("./_site/**/*").pipe(ghPages({ branch: "master" })));
+// clean html
+gulp.task("html-clean", () =>
+  gulp
+    .src("./_site/**/*.html", { base: "./" })
+    .pipe(removeEmptyLines())
+    .pipe(gulp.dest(".")));
 
-gulp.task("default", gulp.parallel("styles", "scripts"));
-gulp.task("deploy", gulp.series(gulp.parallel("styles", "scripts"), "ghPages"));
+// push to github pages
+gulp.task(
+  "ghpages",
+  () => gulp.src("./_site/**/*").pipe(ghPages({ branch: "master" })),
+);
+
+// by default, do everything except push to github pages
+gulp.task("default", gulp.parallel("styles", "scripts", "html-clean"));
+
+// deploy: do everything and push
+gulp.task("deploy", gulp.series("default", "ghpages"));
