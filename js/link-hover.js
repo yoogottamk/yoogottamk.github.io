32881f17814851270fdc1b972ef3d8f55797d8d4
yoogottamk <yoogottamk@outlook.com>
2021-12-07 18:58:34 +0530

    link preview on hover
    using url2og.herokuapp.com (written by me)

---
+++ js/link-hover.js
@@ -0,0 +1,40 @@
+$("document").ready(() => {
+  document.querySelectorAll("a").forEach((el) => {
+    fetch(`https://url2og.herokuapp.com/api/v1/${encodeURIComponent(el.href)}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let { title, image } = data;
+        if (title && image) {
+          el.setAttribute("data-link-title", title);
+          el.setAttribute("data-link-image", image);
+        }
+      }).catch((_err) => {});
+
+    el.addEventListener("mouseover", () => {
+      let offset = el.getBoundingClientRect().left;
+      if (el.getAttribute("data-preview-inserted") == "true") {
+        let preview = el.querySelector(".link-preview");
+        preview.style.display = "block";
+        preview.style.left = `${offset}px`;
+        return;
+      }
+
+      let title = el.getAttribute("data-link-title"),
+        image = el.getAttribute("data-link-image");
+      if (!title || !image) return;
+
+      let html = `
+        <div class="row link-preview" style="left: ${offset}px">
+          <img src="${image}">
+          <p>${title}</p>
+        </div>
+      `;
+      el.insertAdjacentHTML("beforeend", html);
+      el.setAttribute("data-preview-inserted", "true");
+    });
+
+    el.addEventListener("mouseout", () => {
+      el.querySelector(".link-preview").style.display = "none";
+    });
+  });
+});
