fb667950c77274786521d279a1c9619d269ca418
yoogottamk <yoogottamk@outlook.com>
2021-12-14 00:20:12 +0530

    better link hover preview

--- js/link-hover.js
+++ js/link-hover.js
@@ -8,33 +8,22 @@ $("document").ready(() => {
       .then((resp) => resp.json())
       .then((data) => {
         let { title, image } = data;
-        if (title && image) {
-          el.setAttribute("data-link-title", title);
-          el.setAttribute("data-link-image", image);
+        let html = '<div class="row link-preview" style="display: none">';
+        if (image) {
+          html += `<img src="${image}">`;
         }
+        if (title) {
+          html += `<p style="text-align: center">${title}</p>`;
+        }
+        html += "</div>";
+        el.insertAdjacentHTML("beforeend", html);
       }).catch((_err) => {});
 
     el.addEventListener("mouseover", () => {
       let offset = el.getBoundingClientRect().left;
-      if (el.getAttribute("data-preview-inserted") == "true") {
-        let preview = el.querySelector(".link-preview");
-        preview.style.display = "block";
-        preview.style.left = `${offset}px`;
-        return;
-      }
-
-      let title = el.getAttribute("data-link-title"),
-        image = el.getAttribute("data-link-image");
-      if (!title || !image) return;
-
-      let html = `
-        <div class="row link-preview" style="left: ${offset}px">
-          <img src="${image}">
-          <p>${title}</p>
-        </div>
-      `;
-      el.insertAdjacentHTML("beforeend", html);
-      el.setAttribute("data-preview-inserted", "true");
+      let preview = el.querySelector(".link-preview");
+      preview.style.display = "block";
+      preview.style.left = `${offset}px`;
     });
 
     el.addEventListener("mouseout", () => {
